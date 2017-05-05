458e20c88a33ab13f6dcd68edfd3fcff048c2aa4
Chris Martens <martens@csc.ncsu.edu>
2017-05-05 10:52:54 -0400

    some conversational move progress

--- dialogue.js
+++ dialogue.js
@@ -3,17 +3,52 @@
 
 
 var characters =
-  ["Alexis", "Brian", "Corryn"]
+  ["Shaggy", "Velma", "Daphne"]
 
-var things =
-  ["pen", "chair", "coffee", "tea", "notebook"]
+var topics =
+  ["house", "ghost", "murder", "doors"]
 
-var inventory =
-  {
-    "Alexis": [],
-    "Brian": [],
-    "Corryn": []
+var current_topic = "";
+var current_speaker = "";
+
+var presence =
+{
+  "Shaggy": "present",
+  "Velma": "present",
+  "Daphne": "ungreeted",
+}
+
+function related(t1, t2) {
+
+  switch(t1) {
+    case "house": 
+      switch(t2) {
+        case "doors": return true;
+        case "murder": return true;
+        default: return false;
+      }
+    case "ghost":
+      switch(t2) {
+        case "murder": return true;
+        case "doors": return true;
+        default: return false;
+      }
+    case "murder":
+      switch(t2) {
+        case "house": return true;
+        case "ghost": return true;
+        default: return false;
+      }
+    case "doors":
+      switch(t2) {
+        case "house": return true;
+        case "ghost": return true;
+        default: return false;
+      }
+    default: return false;
   }
+
+}
 
 var conversation_log = [];
 
@@ -27,18 +62,8 @@ function choiceToString(c) {
   return str;
 }
 
+/*
 function displayState() {
-  var transcriptAddition = "";
-
-  if(conversation_log.length > 0) {
-    transcriptAddition += conversation_log[conversation_log.length - 1];
-    transcriptAddition += "<br>";
-  }
-  transcriptAddition += "</p>";
-
-  // Append
-  document.getElementById("transcript").innerHTML += transcriptAddition;
-
   var stateHTML = "";
 
   for(c in inventory){
@@ -53,6 +78,7 @@ function displayState() {
   document.getElementById("state").innerHTML = stateHTML;
 
 }
+*/
 
 function displayChoices() {
   // current_choices = generate_choices();
@@ -68,10 +94,9 @@ function begin() { render(""); }
 
 function render(text) {
 
-  document.getElementById("response").innerHTML = text;
+  document.getElementById("transcript").innerHTML += "<br>"+text;
 
   current_choices = generate_choices();
-  displayState();
   displayChoices();
 }
 
@@ -79,7 +104,6 @@ function render(text) {
 function selectChoice(index) {
 
   var display_text = applyOper(current_choices[index]);
-
   
   // current_choices = generate_choices();
   render(display_text);
@@ -99,20 +123,26 @@ function applyOper(cmd) {
   var action = {applies:applies, effects:effects, text:text};
 
   switch(op) {
-    case "take": {
+  /*  case "take": {
       action = take(args[0], args[1]);
       break;
     }
-    case "go": { 
-      action = go(args[0], args[1]);
+  */
+    case "discuss": { 
+      action = discuss(args[0], args[1]);
       break;
     }
-    case "talk": {
-      action = talk(args[0], args[1]);
+    case "change topic": {
+      action = change_topic(args[0], args[1]);
+      break;
+    }
+    case "greet": {
+      action = greet(args[0], args[1]);
       break;
     }
 
   }
+
   if(action.applies) { 
     action.effects();
     displayText = action.text;
@@ -130,16 +160,34 @@ function generate_choices () {
   // XXX use "applies" method on actions?
   for(var ci in characters) {
     var c = characters[ci];
-    for(var ti in things) {
-      var thing = things[ti];
-      choices.push({op:"take", args:[c, thing]});
-    }
+
+    /* Taking things
+      for(var ti in things) {
+        var thing = things[ti];
+        choices.push({op:"take", args:[c, thing]});
+      }
+    */
+
+    // Talking to characters
     for(var oi in characters) {
       var other = characters[oi];
-      if(other != c) {
-        choices.push({op:"talk", args:[c, other]});
+      if(greet(c, other).applies) {
+        choices.push({op:"greet", args:[c, other]});
       }
     } //end loop over other characters
+
+
+    if(discuss(c, current_topic).applies) {
+      choices.push({op:"discuss", args:[c, current_topic]});
+    }
+
+    for(var ti in topics) {
+      var topic = topics[ti];
+      if(change_topic(c, topic).applies) {
+        choices.push({op:"change topic", args:[c,topic]});
+      }
+    } // end loop over topics
+
   } //end outer loop over characters
   return choices;
   
@@ -147,10 +195,84 @@ function generate_choices () {
 
 // Operator specification
 
-// take
+
+// enter
+// preconditions: X absent
+// effects: X not absent; X present; X ungreeted
+
+// exit
+// preconditions: X present
+// effects: X not present; X absent; X ungreeted
+
+// discuss current topic
+function discuss(agent, topic) {
+  var applies = current_topic != "" && current_speaker = "";
+  var text = agent + ": Next quip about " + topic; // XXX
+
+  function effects() {
+    current_topic = topic;
+  }
+
+  return {applies:applies, effects:effects, text:text};
+
+}
+
+function finish_speaking(agent) {
+  var applies = current_speaker == agent;
+
+  function effects() {
+    current_speaker = "";
+  }
+
+  return {applies:applies, effects:effects, text:text};
+}
+
+function change_topic(agent, topic) {
+  var applies = related(topic, current_topic) || current_topic == "";
+  var text = agent + ": Let's talk about the "+topic;
+
+  function effects() {
+    current_topic = topic;
+  }
+
+  return {applies:applies, effects:effects, text:text};
+
+
+}
+
+// greet
+// precondtions: X present; X ungreeted
+// effects: X greeted
+function greet(agent1, agent2) {
+
+  var applies = presence[agent1] == "present"
+    && presence[agent2] == "ungreeted";
+
+  function effects() {
+    presence[agent2] = "present";
+  }
+
+  var text = agent1+" says hello to "+agent2;
+
+  return {applies:applies, effects:effects, text:text};
+
+}
+
+
+/* TODO
+// give
+// preconditions: X has A
+// effects: Y has A
+// turns: 1
+
+// Ask for
+// Drop
+
+// Take
 // preconditions: X and A in same place
 // effect: has X A
 // turns: 1
+/*
 function take(agent, thing) {
 
   var ti = things.indexOf(thing);
@@ -166,28 +288,4 @@ function take(agent, thing) {
   return {applies:applies, effects:effects, text:text};
 
 }
-
-
-
-// talk
-// precondtions: X and Y in L
-// effects: none
-// turns: 1-3
-function talk(agent1, agent2) {
-
-  var applies = true;
-
-  function effects() {
-    var line = agent1+" says hello to "+agent2;
-    conversation_log.push(line);
-  }
-
-  return {applies:applies, effects:effects, text:""};
-
-}
-
-// give
-// preconditions: X has A
-// effects: Y has A
-// turns: 1
-
+*/
